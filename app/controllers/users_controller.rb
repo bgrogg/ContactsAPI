3ce3af7e1407f390fa6f6adb07345bef37b46936
Boris Grogg <borisgrogg@Boriss-MacBook-Air.local>
2016-12-05 18:00:23 -0800

    use the only option for routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,34 +5,34 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: user
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
     else
       render(
-        json: user.errors.full_messages, status: :unprocessable_entity
+        json: @user.errors.full_messages, status: :unprocessable_entity
       )
     end
   end
 
   def show
-    user = User.find(params[:id])
-    render json: user
+    @user = User.find(params[:id])
+    render json: @user
   end
 
   def update
-    user = User.find(params[:id])
-    if user.update_attributes(user_params)
-      render :json => user
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      render :json => @user
     else
-      render :json => user.errors, :status => :unprocessable_entity
+      render :json => @user.errors, :status => :unprocessable_entity
     end
   end
 
   def destroy
-    user = User.find(params[:id])
-    user.destroy
-    render json: user
+    @user = User.find(params[:id])
+    @user.destroy
+    render json: @user
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,5 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  resources :users
+  resources :users, :only => [:create, :destroy, :index, :show, :update]
 end
